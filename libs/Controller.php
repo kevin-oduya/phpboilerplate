089d356590ff69ed0ce6e4d3f2f241b8a9ffbe02
Calvins Ongele <48557897+kevin-oduya@users.noreply.github.com>
2026-04-26 15:32:46 +0300

    Update Controller.php

--- libs/Controller.php
+++ libs/Controller.php
@@ -28,6 +28,13 @@ class Controller {
 		$this->view->currentpage = 0;
 		$this->view->pid = '';  
 		$this->view->page_id = '';
+
+		
+		$allowed_under_maintenance = ['admin', 'login'];
+		if ($this->view->_company['enable_maintenance_mode']) {
+			if (!in_array(CLASS_NAME, $allowed_under_maintenance)) 
+				die($this->maintenance());
+		}
 		  
 	} 
 
@@ -39,6 +46,50 @@ class Controller {
 	}
 	public function _content() {
 		return $this->model->_content();
+	}
+		public function maintenance() {
+		return ("
+			<!doctype html>
+			<head>
+				<title>Site Maintenance</title>
+				<style>
+				article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+				a { color: #dc8100; text-decoration: none; }
+				a:hover { color: #333; text-decoration: none; }
+				article h1, article div{
+				color:black
+				}
+				.center {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				text-align: center;
+				min-height: 100vh;
+				}
+				</style>
+				<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet'  > 
+				</head>
+
+				<body style='background-image:url(/public/assets/system/mainetanance.png);
+
+				background-attachment: fixed;
+						/*background-position: center;*/
+						background-repeat: no-repeat;
+						background-size: cover;
+				'>
+				<div class='center'>
+				<article class=' ' style='background:rgba(255,250,250,0.7); border-radius:6px; padding:15px;' align='center'>
+					<h1>We&rsquo;ll be back soon!</h1><br>
+					<div style='font-size:25px'>
+						<p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always 
+						<a href='mailto:{$this->view->_company['c_email']}'>contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+						<p style='font-size:40px'>&mdash; {$this->view->_company['c_name']}</p>
+					</div><br>
+				</article>
+				</div>
+				</body>
+			
+			");
 	}
    
   
