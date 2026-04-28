e7820fe948641356784f3d8c1c5b5968573b131a
Calvins Ongele <48557897+kevin-oduya@users.noreply.github.com>
2026-04-28 14:45:30 +0300

    Refactor index method to use notFound function

--- controllers/err.php
+++ controllers/err.php
@@ -8,12 +8,7 @@ class Err extends Controller {
 	}
 	
 	function index() { 
-        $this->view->hstyles = array();
-		$this->view->_company = array('c_name'=>$_SERVER['SERVER_NAME'], 'c_logo'=>'1.png', 'c_tel'=>'07');//$this->_company();
-		$this->view->title = '404 Error';
-		//$this->view->render('public/includes/header.inc.php', true);
-		$this->view->render('err/index', true);
-		//$this->view->render(getenv('DOCUMENT_ROOT') . '/public/includes/footer.inc', true);
+        $this->notFound();
 	}
 
 }
